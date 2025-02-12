5b54df5a5c1631ad3142944de91abd51013d544c
MariaRuiz-96 <marijo.ruch1996@gmail.com>
2025-02-12 11:39:45 -0600

    refactor: mejorar estructura de saveNewBook
    - Se eliminó el `addEventListener` del interior de la función `saveNewBook` para mejorar la claridad y la estructura.
    - El `addEventListener` ahora está fuera de la función `saveNewBook`, asignándose directamente al botón "new-book".
    - Se mantiene la funcionalidad de capturar los datos del formulario y crear un nuevo objeto `newBook`.

--- javascript/script.js
+++ javascript/script.js
@@ -6,8 +6,10 @@ document.addEventListener("DOMContentLoaded", function(){
     const nameBook = document.createElement("h3");
     const nameAuthor = document.createElement("h4");
     const section= document.createElement("section");
-    const namePagesRead = document.createElement("h5");
-    const contentPagesRead = document.createElement("span");
+    const namePages = document.createElement("h5");
+    const nameread = document.createElement("h5");
+    const contentPages = document.createElement("span");
+    const contentRead = document.createElement("span");
     const button = document.createElement("button");
 
     function newBook (title, author, pages) {
@@ -18,15 +20,31 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 
     function saveNewBook (){
-        document.getElementById("new-book").addEventListener("click", () => {
+        
             let title = document.getElementById("Title").value;
             let author = document.getElementById("Author").value;
             let pages = document.getElementById("Pages").value;
     
             const booknew = new newBook (title, author, pages)
             console.log(booknew)
-        })
+            createNewBook(booknew) /*se debe de poner aqui adentro ya que debe de pedir datos y despues rellenar*/
     }
 
-    saveNewBook()
+    function createNewBook(book){
+        nameBook.textContent = book.title
+        nameAuthor.textContent = book.author
+        namePages.textContent = "Pagues: "
+
+
+
+        contenedor.appendChild(newContentBook)
+        newContentBook.appendChild(nameBook)
+        newContentBook.appendChild(nameAuthor)
+        section.classList.add("pages")
+        newContentBook.appendChild(section)
+
+    }
+
+
+    document.getElementById("new-book").addEventListener("click", saveNewBook)
 })
