9f39999d7d34d7fcf9dfc44c78c2a4411d15fc17
MariaRuiz-96 <marijo.ruch1996@gmail.com>
2025-02-12 10:49:01 -0600

    feat: agregar función para guardar nuevos libros y estructura inicial en JS
    - Se implementa la función `newBook` para crear objetos de libros con título, autor y páginas.
    - Se agrega la función `saveNewBook` que captura datos del formulario y crea un nuevo libro al hacer clic en el botón "new-book".
    - Se crean elementos dinámicos (`article`, `h3`, `h4`, `section`, `h5`, `span`, `button`) para representar un libro en el DOM.
    - Se incluye un `console.log("cargo bien")` temporal para verificar la carga del script.

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(){
+    console.log("cargo bien")
+
+    const contenedor = document.querySelector("main");
+    const newContentBook = document.createElement("article");
+    const nameBook = document.createElement("h3");
+    const nameAuthor = document.createElement("h4");
+    const section= document.createElement("section");
+    const namePagesRead = document.createElement("h5");
+    const contentPagesRead = document.createElement("span");
+    const button = document.createElement("button");
+
+    function newBook (title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = "No"
+    }
+
+    function saveNewBook (){
+        document.getElementById("new-book").addEventListener("click", () => {
+            let title = document.getElementById("Title").value;
+            let author = document.getElementById("Author").value;
+            let pages = document.getElementById("Pages").value;
     
+            const booknew = new newBook (title, author, pages)
+            console.log(booknew)
+        })
+    }
+
+    saveNewBook()
 })
